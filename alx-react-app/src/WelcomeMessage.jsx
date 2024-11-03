f41f7d1afed7055f16d57b58476bea76cb54f40b
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-03 15:04:05 +0200

    updated

--- alx-react-app/src/WelcomeMessage.jsx
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -1,13 +1,20 @@
 function WelcomeMessage() {
-    render() 
+    
     return (
         <div>
-            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <h1>Welcome to ALX APP!</h1>
             <p>This is a simple JSX component.</p>
-            <p>I am learning about JSX!</p>
+            
             < WelcomeMessage />
         </div>
     );
 };
 
-export default WelcomeMessage;
+export default WelcomeMessage() {
+    return (
+        <div>
+     <h1>Hello everyone, I am learning React at ALX!</h1>
+     <P>I am learning about JSX!</P>
+     </div>
+    );
+}
