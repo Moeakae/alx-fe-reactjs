9fbaef4e5a1631343379df40085a91cc9bd247b5
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-05 16:52:13 +0200

    updated userprofile

---
+++ alx-react-app/src/components/UserProfie.jsx
@@ -0,0 +1,12 @@
+function UserProfile() {
+    const UserProfile = (props) => {
+        return (
+            <div className="user-profile-card">
+                <h2>Namw: {props.name}</h2>
+                <p>Age: {props.age}</p>
+                <p>Bio: {props.bio}</p>
+            </div>
+        );
+};
+};
+export default UserProfile;
