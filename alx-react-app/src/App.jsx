7d126f0d5b6db49407c49a18ecd294c2fb4aad62
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-05 16:35:54 +0200

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import footer from './components/Footer';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 
 import './App.css'
