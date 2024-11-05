42587234cd78a8060dba228c56cae0ef5ebbcd30
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-05 17:08:41 +0200

    updated

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfie';
+import UserProfile from './components/UserProfile';
 import './App.css'
 
 function App() {
