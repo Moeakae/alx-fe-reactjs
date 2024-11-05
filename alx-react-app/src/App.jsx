cd8ef16a186dd993bedfcd2f2d7f7b01922280ac
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-05 16:34:53 +0200

    updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,6 @@
-
-
-
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 
 import './App.css'
@@ -11,7 +11,9 @@ function App() {
   return (
   
       <div>
-        
+        <Header />
+        <MainContent />
+        <Footer />
         <WelcomeMessage />
         
       </div>
