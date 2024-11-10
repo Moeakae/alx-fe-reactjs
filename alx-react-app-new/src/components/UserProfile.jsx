22f064cb61f16e50d08ecec08b093f2a91adabee
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-10 13:59:32 +0200

    updated

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,9 +2,9 @@ import React from "react";
 function UserProfile() {
     const UserProfile = (props) => {
         return (
-            <div className="user-profile-card">
-                <h2>Namw: {props.name}</h2>
-                <p>Age: {props.age}</p>
+            <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }} className="user-profile-card">
+                <h2 style={{ color: 'blue' }}>Name: {props.name}</h2>
+                <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
                 <p>Bio: {props.bio}</p>
             </div>
         );
