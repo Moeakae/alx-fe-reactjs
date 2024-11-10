e651ffaf6ba321ca46fd9e8a7a68e251d9b870ab
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-10 14:35:40 +0200

    updated

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -3,6 +3,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import './App.css'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Footer />
         <WelcomeMessage />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <counter />
       </div>
     
   )
