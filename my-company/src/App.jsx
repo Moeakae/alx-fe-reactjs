7a238a7975cc8a61ee4908a59776b9382eb174fb
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-11 11:17:31 +0200

    updated

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,10 +3,11 @@ import Home from './components/Home';
 import About from './components/About';
 import Services from './components/Services';
 import Contact from './components/Contact';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/home' element={<Home/>} />
         <Route path='/about' element={<About/>} />
