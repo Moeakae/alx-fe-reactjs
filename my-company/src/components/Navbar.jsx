3d26a583adf46ce9ca90650222bf8e5683109cf1
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-11 11:25:01 +0200

    updated

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Links } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div style={{ display: 'flex', padding: '10px', backgroundColor: 'yellow', color: 'white' }}>
+        <div style={{ display: 'flex', padding: '10px', backgroundColor: 'yellow', color: 'white', justifyContent: 'space-between'}}>
             <Links to='/home' style={{ margin: '10px 0', color: 'white' }}>Home</Links>
             <Links to='/about' style={{ margin: '10px 0', color: 'white' }}>About</Links>
             <Links to='/services' style={{ margin: '10px 0', color: 'white' }}>Services</Links>
