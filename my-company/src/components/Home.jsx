bb5feaa73d6cc6f36f1bc236fe77fe0703f8c494
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-11 09:39:12 +0200

    updated

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Welcome to Our Company</h1>
+            <p>We are dedicated to delivering excellence in all our service</p>
+        </div>
+    );
+}
+export default Home;
