1b1ded467b6fbd6632aa06d33b5ab034617014f3
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-14 11:35:39 +0200

    updated

---
+++ recipe-sharing-app/src/components/RecepeList.jsx
@@ -0,0 +1,15 @@
+// RecipeList component
+import { useRecipeStore } from './recipeStore';
+const RecipeList = () => {
+    const recipes = useRecipeStore(state => state.recipes);
+    return (
+        <div>
+        {recipes.map(recipe => (
+            <div key={recipe.id}>
+                <h3>{recipe.title}</h3>
+                <p>{recipe.desciption}</p>
+            </div>
+        ))}
+        </div>
+    );
+};
