0d52499fee105d53c109b6b91e49f55ac743fe33
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-14 12:49:33 +0200

    updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,35 +1,24 @@
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
 
-
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='flex justify-center items-center h-screen'>
+     <div className='w-96 p-6 rounded-1g shadow-md bg-white text-center'>
+      <div className='mb-6'>
+        <h3 className='text-2x1 font- semibold'>Recipes Tracker</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
+     
+      <RecipeList />
+      <AddRecipeForm />
+     </div>
+  
   )
 }
 
-export default App
+export default App;
