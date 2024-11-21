7f83ba0e6849f9ce06400bd8af91c5397a2d552b
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-21 14:29:08 +0200

    updated

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,7 +2,25 @@ import create from 'zustand'
 
 const useRecipeStore = create(set => ({
     recipes: [],
+    searchTerm: '',
+    favorites: [],
+    recommendations: [],
+    filteredRecipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
     setRecipes: (recipes) => set({recipes}),
+    deleteRecipe: (id) => set((state) => ({ recipes: state.recipes.filter((recipe) => recipe.id !==id) })),
+    updateRecipe: (updatedRecipe) => 
 }));
+addFavorite: (recipeId) =>
+    set((state) =>({ favorites: [...state.favorites, recipeId] })),
+removeFavorite: (recipeId) =>
+    set((state) => ({
+        favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+    generateRecommendations: () =>
+        set((state) => {
+            const recommended = state.recipes.filter(
+                (recipe) =>
+            )
+        }
 export default useRecipeStore;
