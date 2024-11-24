e1fbfb6fe3afe63372d91437d906aa3277142260
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-24 17:02:31 +0200

    updated

---
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -0,0 +1,20 @@
+import  useRecipeStore from '../recipeStore';
+const RecommendationsList = () => {
+    const Recommendations = useRecipeStore((state) =>state.Recommendations);
+    const generateRecommendation = useRecipeStore(
+        (state) => state.generateRecommendation
+    );
+    return (
+        <div>
+            <h2>Recommended Recipes</h2>
+            <button onClick={generateRecommendations}>Generate Recommendation</button>
+            {Recommendations.map((recipe) => (
+                <div key={recipe.id}>
+                    <h3>{recipe.title}</h3>
+                    <p>{recipe.description}</p>
+        </div>
+            ))}
+        </div>
+    );
+};
+export default RecommendationsList;
