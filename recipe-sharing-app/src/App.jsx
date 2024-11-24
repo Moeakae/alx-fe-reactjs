94b272308e24789587cc2d130518547821b511a2
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-24 13:16:51 +0200

    updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useRecipeSrore } from "./components/recipeStore";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
-import { BrowserRouter as Router, Routes, Route } from 'react- router-dom';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
 import DeleteRecipe from './components/DeleteRecipeButton';
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,7 +1,7 @@
 import { link } from 'react-router-dom';
 import  useRecipeStore from './recipeStore';
 const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+    const recipes = useRecipeStore((state) => state.filteredRecipes);
     return (
         <div>
         {recipes.map(recipe => (
