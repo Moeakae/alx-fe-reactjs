2c398b88ff046f2c88e6409fdf58a4933ef27229
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-11-29 20:20:26 +0200

    updated

--- form-handling-react/src/components/FormikForm.js
+++ form-handling-react/src/components/FormikForm.js
@@ -7,5 +7,40 @@ const formik = () => {
             username: "",
             email: "",
             password: "",
-    })
-}
+},
+validationSchema: Yup.object({ 
+    username: Yup.string().required("Username is required"),
+    email: Yup.string()
+    .email("Invalid email addresss")
+     .required("email is required"),
+    password: Yup.string()
+    .min(6, "password must be at least 6 characters")
+     .required("password is required"),
+}),
+onSubmit: (values) => { 
+    console.log("Form values:", values)
+    alert("User registered successfully!");
+},
+});
+return (
+    <form onSubmit={formik.handleSubmit}>
+        <h2>User Registration</h2>
+        <div>
+            <label>Username:</label>
+            <input type="text" name="username" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.value.username} />
+            {formik.touched.email && formik.errors.email ? (
+                <p style={{ color: 'red' }}>{formik.errors.email}</p>
+            ) :null}
+        </div>
+        <div>
+            <label>Password:</label>
+            <input type="password" name="username" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.password} />
+            {formik.touched.password && formik.errors.password ? (
+                <p style={{ color: 'red'}}>{formik.errors.password}</p>
+            ) : null}
+        </div>
+        <button type="submit">Register</button>
+    </form>
+);
+};
+export default FormikForm;
