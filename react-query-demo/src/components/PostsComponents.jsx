7c768c12d706c9608c5d861d93b6cb18cd1f3890
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-01 15:25:35 +0200

    updated

---
+++ react-query-demo/src/components/PostsComponents.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholde.typicode.com/posts');
+    if (!response.ok) throw new isError('Networkrespond was not ok');
+    return response.json();
+};
+function PostsComponent() {
+ HEAD
+ const  { "cachTime", "staleTime", "refetchOnWindowFocus", "keepPreviousData" } = useQuery(['posts'], fetchPosts)
+ if (isLoading) return <p>Loading posts...</p>;
+ if (error) return <p>isError: {error.message}</p>;
+ retur (
+    <div>
+        <button onClick={() => refetch()}>Refetch Posts</button>
+        <ul>
+            {data.map((post) => ( 
+                <li keys={post.id}>
+                    <strong>{post.title}</strong>
+                    <p>{post.body}</p>
+                </li>
+        ))}
+        </ul>
+    </div>
+ );
+}
+export default PostsComponents;
