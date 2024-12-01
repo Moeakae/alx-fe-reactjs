89376749fd1b85f57985e07fe3874eb95c4a858c
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-01 20:59:22 +0200

    updated

---
+++ react-todo/src/_tests_/TodoList.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render, screen, fireEvent } from  '@testing-library/react';
+ import '@testing-library/jest-dom';
+ import TodoList from "../components/TodoList";
+
+ test('render initial todos', () => {
+    render(<Todolist />);
+    expect(screen.getByText('Learn React')).toBeInTheDocument();
+    expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
+ });
+ test('adds a new todo', () => {
+     render(<TodoList />);
+     const input = screen.getByPlaceholderText('Add a new todo');
+     const addButton = screen.getByText('Add');
+     expect(screen.getByText('New Todo')).toBeInTheDocument();
+ });
+ test('toggles a todo', () => {
+    render(<TodoList />);
+    const todoItem = screen.getByText('Learn React');
+    expect(todoItem).not.toHaveStyle('text-decoration: line-through');
+    fireEvent.click(todoItem);
+    expect(todoItem).toHaveStyle('text-decoration: linr-through');
+
+ });
+ test('delete a todo', () => { 
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText('Delete')[0];
+    fireEvent.click(deleteButton);
+    expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+ });
