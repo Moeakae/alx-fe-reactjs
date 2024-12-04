4f58189e65e0ddbdbee19993b7d6ed9255c43b83
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-04 17:56:12 +0200

    updated

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
   theme: { extend: {} },
-  varients: { extend: {} },
+  variants: { extend: {} },
   
   plugins: [],
 
