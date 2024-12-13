94fdb437f8fbc36819c98efd5a037261818d3f43
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-13 20:54:18 +0200

    updated

---
+++ github-user-search/src/services/Githubservice.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const fetchUserData = async (username) => {
+    const respond = await axios.get('https://api.github.com/users/${username}');
+    return respond.data;
+}
+export { fetchUserData };
