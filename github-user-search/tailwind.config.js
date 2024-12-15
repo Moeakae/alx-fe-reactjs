5076009d78e0b43be809fce5cc5cab401d620baa
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-15 18:58:39 +0200

    updated

--- github-user-search/tailwind.config.js
+++ github-user-search/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/**/*.{js,jsx,tsx}','./public/index.html'],
+  darkMode: false, // you can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
