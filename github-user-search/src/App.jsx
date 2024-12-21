4393e483159f593086ea00f163d104a6fbdb7577
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2024-12-21 22:03:26 +0200

    updated

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
-
+import githubService from "./services/Githubservice";
 
 
 function App() {
   return ( 
     <div>
       <Search />
+      <githubService />
     </div>
     
   );
